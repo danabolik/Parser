ed9140046be067a1a404fa30230cea7fe9e68d7e
danabolik <kvaciy2905@gmail.com>
2022-04-03 14:39:30 +0300

    P-1, feat: added config for migrations

---
+++ app/scripts/migrations/20220403142457-create_file_data_table.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+CREATE TABLE file_data (
+    id int not null,
+    file_name varchar(255) NOT NULL,
+    extension varchar(4) NOT NULL,
+    `data` text NOT NULL,
+    parsing_date  timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- +migrate Down
+DROP TABLE file_data;
