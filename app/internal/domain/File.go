033a17de3a07bbd3e1465f470c746c9e9945b53e
danabolik <kvaciy2905@gmail.com>
2022-04-03 18:24:28 +0300

    P-1, feat: extends file structure

--- app/internal/domain/File.go
+++ app/internal/domain/File.go
@@ -1,7 +1,38 @@
 package domain
 
+type File interface {
+	GetId() uint
+	GetFileName() string
+	SetFileName(fileName string)
+	GetExtension() string
+	SetExtension(ext string)
+}
+
+func (f CsvFile) GetId() uint {
+	return f.Id
+}
+
+func (f CsvFile) GetFileName() string {
+	return f.fileName
+}
+func (f *CsvFile) SetFileName(fileName string) {
+	f.fileName = fileName
+}
+
+func (f CsvFile) GetExtension() string {
+	return f.extension
+}
+func (f *CsvFile) SetExtension(ext string) {
+	f.extension = ext
+}
+
 type CsvFile struct {
-	FileName  string
-	Extension string
-	Data      string
+	Id        uint
+	fileName  string
+	extension string
+}
+
+type FileRow struct {
+	FileId uint
+	Data   string
 }
